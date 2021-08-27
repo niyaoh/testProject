9c6ff0f12048bae7f23cc4e047c53d22ec6d8e13
niyaoh <doyoonkim980818@gmail.com>
2021-08-27 21:33:44 +0900

    제대로 세팅된 기본 프로젝트입니다

--- src/testProject/Main.java
+++ src/testProject/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("회원 프로그램입니다.");
+		System.out.println("1.회원저장 2.회원보기 3.로그인 4.로그아웃 5.게시판열기");
 		int select = sc.nextInt();
 		
 		while(true) {
