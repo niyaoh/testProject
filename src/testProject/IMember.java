e1f7162033ba61edcd7f920cbd981081f5eca75f
niyaoh <doyoonkim980818@gmail.com>
2021-08-27 21:20:42 +0900

    회원 프로그램 기본입니다.

---
+++ src/testProject/IMember.java
@@ -0,0 +1,5 @@
+package testProject;
+
+public interface IMember {
+
+}
---
+++ src/testProject/Main.java
@@ -0,0 +1,32 @@
+package testProject;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("회원 프로그램입니다.");
+		int select = sc.nextInt();
+		
+		while(true) {
+			switch(select) {
+			case 1 :
+					break;
+			case 2 :
+					break;
+			case 3 :
+					break;
+			case 4 :
+					break;
+			case 5 :
+					break;
+			}
+		}
+		
+		
+	}
+
+}
