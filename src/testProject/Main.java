97f8e06b409fe8fe89a870fcd65fd14150e9700d
raina <skgus5598@gmail.com>
2021-08-27 22:17:04 +0900

    로그아웃기능

--- src/testProject/Main.java
+++ src/testProject/Main.java
@@ -2,15 +2,21 @@ package testProject;
 
 import java.util.Scanner;
 
+import raina.logout;
+import raina.logoutInterface;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		logoutInterface li = new logout();
+		
 		System.out.println("회원 프로그램입니다.");
 		System.out.println("1.회원저장 2.회원보기 3.로그인 4.로그아웃 5.게시판열기");
 		int select = sc.nextInt();
+		
 		
 		while(true) {
 			switch(select) {
@@ -21,6 +27,8 @@ public class Main {
 			case 3 :
 					break;
 			case 4 :
+					li.display();
+					
 					break;
 			case 5 :
 					break;
