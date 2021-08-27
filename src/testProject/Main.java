0056fc1a745a6432a3ccb4c44e5c8f3a7f067a9e
niyaoh <doyoonkim980818@gmail.com>
2021-08-28 00:00:48 +0900

    회원보기+로그인+로그아웃+게시판 기능 구현

--- src/testProject/Main.java
+++ src/testProject/Main.java
@@ -8,9 +8,6 @@ import raina.logout;
 import raina.logoutInterface;
 import sunwoo.MemberView;
 
-
-
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -20,19 +17,15 @@ public class Main {
 		MemberView View=new MemberView();
 		Scanner sc = new Scanner(System.in);
 		
-		
-		
-		System.out.println("회원 프로그램입니다.");
-		System.out.println("1.회원저장 2.회원보기 3.로그인 4.로그아웃 5.게시판열기");
-		int select = sc.nextInt();
-		
-		
 		while(true) {
+			System.out.println("회원 프로그램입니다.");
+			System.out.println("1.회원저장 2.회원보기 3.로그인 4.로그아웃 5.게시판열기");
+			int select = sc.nextInt();
+			
 			switch(select) {
 			case 1 :
 					break;
-			case 2 :
-				View.display();         //김선우
+			case 2 :View.display();        //김선우
 					break;
 			case 3 :alan.display();		//강동원
 					break;
